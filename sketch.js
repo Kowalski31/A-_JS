e21588694648fd9e398166599f5449147a66ee48
Kowalski31 <laikhanhtoanb10@gmail.com>
2024-03-08 21:32:03 +0700

    add adjList

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
 var wall = 'rgb(255, 255, 255)';
-var original = 'rgb(243, 190, 190)'
+var original = 'rgb(243, 190, 190)';
+var maze_length = 10;
+
 
 function setup() {
     var maze_container = document.getElementById('maze_container');
 
-    for (var i = 0; i < 10; i++){
+    for (var i = 0; i < maze_length; i++){
         var row = document.createElement('div');
         row.className = 'row row' + (i+1);
         row.id = 'row' + (i+1);
 
-        for (var j = 0; j < 10; j++){
+        for (var j = 0; j < maze_length; j++){
             var node = document.createElement('div');
             node.className = 'node node' + ((i*10) + (j+1));
             node.id = 'node' + ((i*10) + (j+1));
@@ -39,8 +41,69 @@ function toggleBackgroundColor(elementID){
 }
 
 function clearMaze(){
-    for(var i = 2; i < 10*10; i++){
+    for(var i = 2; i < maze_length*maze_length; i++){
         var node = document.getElementById('node' + i);
         node.style.backgroundColor = original;
     }
 }
+
+function startMaze(){
+
+    var maze = [];
+
+    for (let i = 0; i < maze_length; i++){
+        maze[i] = new Array(maze_length).fill(0);
+    }
+
+    var rowCount = 0;
+    var colCount = 0;
+    var nodeVal = 1;
+
+    for (let i = 1; i <= maze_length*maze_length; i++){
+        var node_color = document.getElementById('node' + i).style.backgroundColor;
+        if(node_color == wall){
+            maze[rowCount][colCount] = -1;
+        }
+        else{
+            maze[rowCount][colCount] = nodeVal;
+            nodeVal++;    
+        }
+
+        colCount++;
+
+        if(colCount == maze_length){
+            colCount = 0;
+            rowCount++;
+        }
+    }
+    // console.log(maze);
+    
+    var adjList = {};
+
+    var possibleMoves = [[-1, 0], [0, -1], [1, 0], [0, 1]]; // up, left, down, right
+
+    for (let i = 0; i < maze_length; i++){
+        for (let j = 0; j < maze_length; j++){
+            if(maze[i][j] != -1){
+                var node = maze[i][j];
+                // adjList[node] = [];
+                var neighbors = [];
+
+                for (let k = 0; k < 4; k++){
+                    var newRow = i + possibleMoves[k][0];
+                    var newCol = j + possibleMoves[k][1];
+
+                    if(newRow >= 0 && newRow < maze_length && newCol >= 0 && newCol < maze_length){
+                        if(maze[newRow][newCol] != -1){
+                            // adjList[node].push(maze[newRow][newCol]);
+                            neighbors.push([newRow, newCol]);
+                        }
+                    }
+                }
+                adjList[node] = neighbors;
+            }
+        }
+    }
+    
+    console.log(adjList);
+}   
